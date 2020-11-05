a68eb6e31137e88d0b67378d309ed64b8fcdb4a7
Cole Patterson <seaborncpatterson@gmail.com>
2020-11-05 11:48:34 -0600

    finished exercise

---
+++ StateExercise2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StateExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var celcius = TempConverter.FahrenheitToCelsius(68);
+            var fahrenheit = TempConverter.CelciusToFahrenheit(20);
+
+            Console.WriteLine("After conversion:");
+            Console.WriteLine($"celcius {celcius}");
+            Console.WriteLine($"fahrenheit {fahrenheit}");
+        }
+    }
+}
---
+++ StateExercise2/StaticExercise2
@@ -0,0 +1,22 @@
+﻿using System;
+namespace StateExercise2
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double fahrenheight)
+        {
+           var result = (fahrenheight - 32) / 1.8;
+
+            return result; 
+        }
+
+        public static double CelciusToFahrenheit(double celsius)
+        {
+            return (celsius * 9) / 5 + 32;
+        }
+    }         
+
+
+            
+    
+}
